a50a145d26c3b057f4447880cae8625305f2b71d
YoFionn <ericksoncooperation@gmail.com>
2021-08-29 19:45:33 +0200

    new start on learning some simple ai stuff

--- Interioer/Assets/Scenes/AI_Stuff/EnemyController.cs
+++ Interioer/Assets/Scenes/AI_Stuff/EnemyController.cs
@@ -3,29 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class EnemyController : MonoBehaviour{
+public class EnemyController : MonoBehaviour
+{
 
     public float lookRadius = 10f;
-    Transform target;
+    public bool playerInSightRange;
+    public Transform target;
     NavMeshAgent agent;
+    public LayerMask whatIsGround, whatIsPlayer;
+
 
 
     // Start is called before the first frame update
-    void Start(){ 
-        target = PlayerManager.instance.player.transform;
-        agent = GetComponent<NavMeshAgent>();
+    private void Awake()
+    {
+        target = GetComponent<Transform>();
     }
 
     // Update is called once per frame
-    void Update(){
+    void Update()
+    {
+        playerInSightRange = Physics.CheckSphere(transform.position, lookRadius, whatIsPlayer);
+        if (playerInSightRange) Chasing();
+/*         target = GetComponent<Transform>();
         float distance = Vector3.Distance(target.position,transform.position);
         if (distance <= lookRadius){
             agent.SetDestination(target.position);
-        }
+        } */
     }
 
-    void OnDrawGizmos() {
+    private void Chasing(){
+        agent.SetDestination(target.position);
+    }
+
+/*     void OnDrawGizmos()
+    {
         Gizmos.color = Color.red;
         Gizmos.DrawSphere(transform.position, lookRadius);
-    }
+    } */
 }
